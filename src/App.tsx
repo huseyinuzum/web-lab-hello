ccd858604c617c42f5a4c28ada15b009c5ff3fe8
Hüseyin Üzüm <147386690+huseyinuzum@users.noreply.github.com>
2026-02-27 23:22:50 +0300

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+      <h1>WEB TASARIM VE PROHRAMLAMA </h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Hüseyin Üzüm</p>
+      <p>Ogrenci No: 235541049</p>
+   </div>
+  )
+}
+
+export default App
